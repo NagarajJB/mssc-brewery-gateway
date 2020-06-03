0cc30a329bbabb3c5a9913196592f3cf202407dc
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-03 20:29:55 +0530

    GoogleConfig.java

---
+++ src/main/java/com/njb/gateway/config/GoogleConfig.java
@@ -0,0 +1,22 @@
+package com.njb.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("udemy")
+@Configuration
+public class GoogleConfig {
+
+	@Bean
+	public RouteLocator googleRouterConfig(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/googlesearch")
+						.filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+						.uri("https://www.google.com/").id("google"))
+				.build();
+	}
+
+}
