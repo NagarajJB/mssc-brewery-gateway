6d1d8cb0481db3ec895cfa14a4c8d59bb451c8bd
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-03 18:03:23 +0530

    initial commit

---
+++ src/main/java/com/njb/gateway/MsscBrewaryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.njb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewaryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewaryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njb/gateway/MsscBrewaryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njb.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewaryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
